8b3f37ab0f58ba77806ceb5ce2d661189772a5ad
nikolay <nikolaykrkn@gmail.com>
2016-10-31 13:38:07 +0200

    working version

--- BatchCalculator/views.py
+++ BatchCalculator/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from BatchCalculator.models import rawMaterial
 from django.views.generic import ListView
 
-class rawMaterialView(ListView):
-    model = rawMaterial
+#class rawMaterialView(ListView):
+    #model = rawMaterial
     #response = [rawMaterial.materialName, rawMaterial.contentAl2O3, rawMaterial.contentAl2O3, rawMaterial.contentNa2O]
 
 
-#def index(request):
- #   return render(request, 'BatchCalculator/index.html')
+def index(request):
+    return render(request, 'BatchCalculator/index.html')
--- GlassSite/urls.py
+++ GlassSite/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-	url(r'^', include('BatchCalculator.urls'), rawMaterialView.as_view()),
+	url(r'^', include('BatchCalculator.urls')),
     url(r'^admin/', admin.site.urls),
 ]
