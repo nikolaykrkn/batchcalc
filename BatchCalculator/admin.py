ff8db379ac4c3f904eaddcceeab73be15ec0738b
nikolay <nikolaykrkn@gmail.com>
2016-10-31 13:34:50 +0200

    first

---
+++ BatchCalculator/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import rawMaterial
+
+
+# Register your models here.
+admin.site.register(rawMaterial)
---
+++ BatchCalculator/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BatchcalculatorConfig(AppConfig):
+    name = 'BatchCalculator'
